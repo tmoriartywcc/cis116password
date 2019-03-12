6a72797ff42f4b965dcabc60fc9bb4525e268e10
Timothy J. Moriarty <X00281296@wcc.dom>
2019-03-12 11:49:22 -0500

    added functions

--- password.py
+++ password.py
@@ -1,8 +1,13 @@
 PASSWORD = 'Waubonsee'
 
-user_pass = input('Please enter password: ')
+def check_password(user_pass):
+    if user_pass == PASSWORD:
+        print('Correct!')
+    else:
+        print('Incorrect!')
 
-if user_pass == PASSWORD:
-    print('Correct!')
-else:
-    print('Incorrect!')
+def main():
+    user_pass = input('Please enter password: ')
+    check_password(user_pass)
+
+main()
